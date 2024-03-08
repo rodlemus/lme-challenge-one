b7d42a7c3583e8f215ff1e1841dbc0f5212657b0
Call Center <rodlemus30@gmail.com>
2024-03-08 08:55:46 -0600

    inicializar marca por defecto y cambiar estilo de marca al clickear cada una y deshabilitar la anterior

--- public/js/search-cars/search-cars.js
+++ public/js/search-cars/search-cars.js
@@ -1,9 +1,43 @@
 const params = new Proxy(new URLSearchParams(window.location.search), {
   get: (searchParams, prop) => searchParams.get(prop),
 });
-console.log(params.category);
 const brandsContainersElement = document.getElementById("brandsRowContainer");
+const brandsItems = Array.from(brandsContainersElement.children);
 
-console.log(brandsContainersElement.children.forEach(element => {
-    
-});[0].children[1].textContent);
+const initDefaultBrand = (defaultQueryUrl) => {
+  brandsItems.forEach((brandElement) => {
+    const brandTextFromSpan = brandElement.children[1].textContent
+      .trim()
+      .toLowerCase();
+
+    const brandMatch = brandTextFromSpan === defaultQueryUrl;
+    if (brandMatch) {
+      brandElement.classList.add("brand-item-selected");
+    }
+  });
+};
+
+const removeBrandSelected = () => {
+  brandsItems.forEach((brandElement) => {
+    const isCurrentBrandSelected = brandElement.classList.contains(
+      "brand-item-selected"
+    );
+    if (isCurrentBrandSelected) {
+      brandElement.classList.remove("brand-item-selected");
+    }
+  });
+};
+const defaultQueryBrand = !params.defaultBrand ? "toyota" : params.defaultBrand;
+
+// Esta funcion primero lee el Query Param ' defaultBrand ', para definir una marca de carros por defecto
+// sino existe en la url se asigna una por default en este caso toyota, y se deja el Div por default con la clase de css
+// brand-item-selected
+
+initDefaultBrand(defaultQueryBrand);
+
+brandsItems.forEach((brandElement) => {
+  brandElement.addEventListener("click", () => {
+    removeBrandSelected();
+    brandElement.classList.add("brand-item-selected");
+  });
+});
