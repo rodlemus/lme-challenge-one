1b03b77c3da60fb30eccd95b94125296fd926fd4
Call Center <rodlemus30@gmail.com>
2024-03-12 14:00:34 -0600

    alert removido y toast agregado

--- public/js/search-cars/search-cars.js
+++ public/js/search-cars/search-cars.js
@@ -43,21 +43,17 @@ brandsItems.forEach((brandElement) => {
 });
 
 // Alerts del boton guardar auto
-
-const hiddenAlertsElements = Array.from(
-  document.getElementsByClassName("alert-absolute")
-);
+const toastElement = document.getElementById("liveToast");
+const toastComponent = new bootstrap.Toast(toastElement, {
+  autoHide: true,
+  delay: 2000,
+});
 const saveCarButtonsElements =
   document.getElementsByClassName("saveCar-button");
 
 Array.from(saveCarButtonsElements).forEach((saveCarButton, index) => {
   saveCarButton.addEventListener("click", () => {
-    const alertFromCurrentCard = hiddenAlertsElements[index];
-    alertFromCurrentCard.classList.remove("hide");
-    alertFromCurrentCard.classList.add("show");
-    setTimeout(() => {
-      alertFromCurrentCard.classList.remove("show");
-      alertFromCurrentCard.classList.add("hide");
-    }, 700);
+    console.log("show toast");
+    toastComponent.show();
   });
 });
